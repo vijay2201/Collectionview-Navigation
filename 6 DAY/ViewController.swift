3052c30a0dcd082730dcb66e858a102ab1e749b8
COE-008 <coe-008@coe-008s-imac.local>
2019-12-31 13:43:47 +0530

    Initial Commit

---
+++ 6 DAY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  6 DAY
+//
+//  Created by COE-008 on 31/12/19.
+//  Copyright © 2019 COE-008. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
